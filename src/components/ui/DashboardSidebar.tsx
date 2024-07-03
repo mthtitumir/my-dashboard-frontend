84bb8119f7c40402f5c5aad8886d26f918cf393f
mthtitumir <titumirgccc@gmail.com>
2024-07-03 19:05:32 +0600

    dashboar sidebar setup

---
+++ src/components/ui/DashboardSidebar.tsx
@@ -0,0 +1,62 @@
+import { icons } from "../../icons";
+import { useLocation } from "react-router-dom";
+
+const DashboardSidebar = () => {
+  const sidebarData = [
+    {
+      icon: icons.dashboard.blogs,
+      name: "All Blogs",
+      path: "/dashboard/all-blogs",
+    },
+    {
+      icon: icons.dashboard.projects,
+      name: "All Projects",
+      path: "/dashboard/all-projects",
+    },
+    {
+      icon: icons.dashboard.newBlog,
+      name: "New Blog",
+      path: "/dashboard/add-new-blog",
+    },
+    {
+      icon: icons.dashboard.newProject,
+      name: "New Project",
+      path: "/dashboard/add-new-project",
+    },
+    {
+      icon: icons.dashboard.home,
+      name: "Go Home",
+      path: "/",
+    },
+  ];
+  const location = useLocation();
+  const { pathname } = location;
+
+  return (
+    <div className="hidden lg:block lg:col-span-2 bg-[#0A192F] rounded-lg p-8 border-main h-[calc(100vh-64px)] sticky top-12 z-50">
+      <div>
+        <h1 className="text-center text-xl text-white ">M. T. H. TITUMIR</h1>
+      </div>
+      <div className="h-px bg-gray-500 mt-1 mb-4"></div>
+      <div className="flex flex-col gap-3">
+        {sidebarData?.map((sd, index) => (
+          <a key={index} href={sd.path}>
+            <div
+              key={index}
+              className={`${
+                sd.path === pathname
+                  ? "text-sky-500 border border-sky-500"
+                  : "text-inherit border-main"
+              } text-sm md:text-sm lg:text-md uppercase tracking-wider flex items-center gap-2 rounded-sm px-3 py-1`}
+            >
+              <sd.icon size={20} />
+              <h1>{sd.name}</h1>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardSidebar;
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,7 +1,39 @@
-const DashboardLayout = () => {
-  return (
-    <div>DashboardLayout</div>
-  )
-}
+import { Outlet } from "react-router-dom";
+import DashboardSidebar from "../components/ui/DashboardSidebar";
+import { verifyToken } from "../utils/jwt";
 
-export default DashboardLayout
+const DashboardLayout = () => {
+  const token = localStorage.getItem("token");
+  const data = verifyToken(token as string);
+  console.log(data);
+  if (!data) {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-red-600 text-2xl">
+        You Are Unauthorized 👿
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4 lg:p-8 text-gray-400 c-auto ">
+      <div className="grid grid-cols-10 gap-6">
+        {/* left box  */}
+        <DashboardSidebar />
+
+        {/* right box  */}
+        <div className="col-span-12 lg:col-span-8 bg-[#0A192F] rounded-lg lg:rounded-lg border-main h-[calc(100vh-32px)] lg:h-[calc(100vh-64px)] flex flex-col relative">
+          {/* navbar  */}
+          {/* <ProfileToggle /> */}
+          {/* <div className="flex-shrink-0">
+            <Navbar position="top" />
+          </div> */}
+          <div className="flex-grow p-4 overflow-auto ">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
